2071f5901a83feb8167ba04429169ff44140d100
Roman Pogribnyi <pogribnyi@gmail.com>
2014-07-12 16:00:50 +0200

    setting for debugging the linking issue

--- source/blender/python/manta_pp/pwrapper/manta_api.cpp
+++ source/blender/python/manta_pp/pwrapper/manta_api.cpp
@@ -1,14 +1,30 @@
 #include "Python.h"
-#include "manta_api.h"
+//#include "manta_api.h"
 #include "../manta.h"
+#include "../general.h"
+namespace Manta {
+	extern void guiMain(int argc, char* argv[]);
+	extern void guiWaitFinish();
+}
+
+using namespace std;
+using namespace Manta;
+
+#if PY_MAJOR_VERSION >= 3
+typedef wchar_t pyChar;
+typedef wstring pyString;
+#else
+typedef char pyChar;
+typedef string pyString;
+#endif
 
 //#ifdef __cplusplus
-//extern "C" {
+extern "C" {
 //#endif
-	PyObject * PyInit_Manta(void)
-	{
-		return Pb::PyInit_Main();
-	}
+PyObject * PyInit_Manta(void)
+{
+	return Pb::PyInit_Main();
+}
 //#ifdef __cplusplus
-//}
+}
 //#endif
